0ea6069f794a4e52571b5e96d052ca928e164558
Fannan Luc Coulibaly <fannancoulibaly@arolitec.com>
2021-09-09 09:41:26 +0000

    Add todo postgres sql datasource

---
+++ src/datasources/todo.datasource.ts
@@ -0,0 +1,31 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'todo',
+  connector: 'postgresql',
+  url: '',
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'arolitec',
+  database: 'todos'
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class TodoDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'todo';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.todo', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
